10eb4d046bdcb0101f7e91a64bbd474a13e6e13d
Mohsen Mesgar <mesgar@ukp.informatik.tu-darmstadt.de>
2022-01-31 16:25:28 +0100

    Update 5_Peer_Graded_Assignment_Questions.py

--- 5_Peer_Graded_Assignment_Questions.py
+++ 5_Peer_Graded_Assignment_Questions.py
@@ -99,7 +99,7 @@ app.layout = html.Div(children=[
                                         ),
                                         # TASK2: Add a dropdown
                                         # Enter your code below. Make sure you have correct formatting.
-                                          dcc.Dropdown(id='input-type.', 
+                                          dcc.Dropdown(id='input-type', 
                                                         options=[
                                                             {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
                                                             {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}
